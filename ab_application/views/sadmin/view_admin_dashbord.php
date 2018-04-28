7601d4ebd144754e0aa03d118b05d9466997f012
sohelbijay123 <38805154+sohelbijay123@users.noreply.github.com>
2018-04-28 14:06:48 +0600

    Update view_admin_dashbord.php

--- ab_application/views/sadmin/view_admin_dashbord.php
+++ ab_application/views/sadmin/view_admin_dashbord.php
@@ -4,7 +4,7 @@
         <!--=== Breadcrumbs ===-->
         <div class="breadcrumbs">
             <div class="container">
-                <h1 class="pull-left"> Company List Test </h1>
+                <h1 class="pull-left"> Company List Test1 </h1>
                 <ul class="pull-right breadcrumb">
                     <!--                <li><a href="index.html">Home</a></li>
                                     <li><a href="">Pages</a></li>
